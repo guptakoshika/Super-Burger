fd29e187804d8250c9e084c3c0c6c3ce4f2a00cf
Koshika Gupta <koshika1464.cse16@chitkara.edu.in>
2018-04-16 19:27:20 +0530

    Update SuperBurger.cpp

--- SuperBurger.cpp
+++ SuperBurger.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 class functions{
 	public:
+		// after order if confirmed the bill to be displayed.
 	int displaybill(int bu,int ch)
 	{
 		cout<<"Number of burgers ordered: "<< bu << endl;
-		if(ch==1)
+		if(ch==1)     //if number of slices are 1
 		{
 			cout<<"Total bill:  "<<bu*30<<endl;
 			return bu*30;
 		}
-		else if(ch==2)
+		else if(ch==2)    //if number of cheese slices are 2
 		{
 			cout<<"Total bill:  "<<bu*40<<endl;
 			return bu*40;
 		}
 	}
+	//for user input what to be done!
    	int mainmenu(){
 	   int n;
 		cout << "\t \t \t \t \t ";
@@ -39,85 +41,102 @@ class functions{
 		cin>>n;
 		return n;
 	}
-	int order(int buns,int pattes,int cheese)
+	int order(int *buns,int *pattes,int *cheese)
 {
 	int bu,ch,n;
 	char c;
-	cout<<"Enter the no. of burgers"<<endl;
+	cout << "Enter the no. of burgers" << endl;
 	cin>>bu;
-	while(buns<2*bu)
+	while(*buns<2*bu)
 	{
-		cout<<"Not enough buns in stock, Buns left:"<<" "<<buns<<" Re-enter burger's according to the stock";
+		cout<<"Not enough buns in stock, Buns left:"<<" "<< *buns <<" Re-enter burger's according to the stock";
+		cin>>bu; 
+	}
+	*buns = *buns-2*bu;
+	while(*pattes<bu)
+	{
+		cout<<"Not enough pattes in stock, Pattes left:"<<" "<< *pattes <<" Re-enter the burger's according to the stock"<<endl;
 		cin>>bu;
 	}
-		buns = buns - 2;
-	while(pattes<bu)
-	{
-		cout<<"Not enough pattes in stock, Pattes left:"<<" "<<pattes<<" Re-enter the burger's according to the stock"<<endl;
-		cin>>bu;
-	}
-		pattes = pattes - 1;
+	*pattes = *pattes-1*bu;
 	cout<<"Enter choice(1 for single cheese slice or 2 for double cheese)"<<endl;
 	cin>>ch;
 	while(ch<1||ch>2)
 	{
-	cout<<"Invalid choice,Re-enter it"<<endl;
+	cout << "Invalid choice,Re-enter it" << endl;
 	cin>>ch;
 	}
 	if(ch==1)
 	{
-		while(cheese<bu)
+		while(*cheese<bu)
 		{
-			cout<<"Not enough cheese slices in stock,cheese slices left: "<<cheese<<" Re-enter the burger according to the stock"<<endl;
+			cout << "Not enough cheese slices in stock,cheese slices left: "<< *cheese << " Re-enter the burger according to the stock" << endl;
 			cin>>bu;
-			cheese = cheese -1;
 		}
+		*cheese = *cheese-1*bu;
 	}
 	if(ch==2)
 	{
-		while(cheese<2*bu)
+		while(*cheese<2*bu)
 		{
-			cout<<"Not enough cheese slices in stock,cheese slices left: "<<cheese<<" Re-enter the burger according to the stock"<<endl;
+			cout << "Not enough cheese slices in stock,cheese slices left: "<< *cheese << " Re-enter the burger according to the stock" << endl;
 			cin>>bu;
-			cheese = cheese-2;
 		}
+		*cheese = *cheese-2*bu;
+
 	 }
 	 cout << "Confirm Order?" ;
 	 cin >> c;
 	 if(c=='y')
 	 {
 	 	n=displaybill(bu,ch);	
-		 return n;
+	 	return n;
 	 }
-		else
-		{
-			int order(int buns,int pattes,int cheese);
-		}
-
+	 else
+	      int order (int *buns, int *pattes , int *cheese);
 }
+// to check the stock left.
 void dstock(int buns,int pattes, int cheese)
 {
-	cout<<"Number of buns left:"<<buns<<endl;
-	cout<<"Number of patties left:"<<pattes<<endl;
-	cout<<"Number of cheese slices left:"<<cheese<<endl;
+	cout<<"Number of buns left: "<<buns<<endl;
+	cout<<"Number of patties left: "<<pattes<<endl;
+	cout<<"Number of cheese slices left: "<<cheese<<endl;
 }
-
+	void rec(int *bun, int *pat , int *che)
+{
+	int b,p,c;
+	cout<<"Enter number of buns"<<endl;
+	cin>>b;
+	*bun+=b;
+	cout<<"Enter number of pattes"<<endl;
+	cin>>p;
+	*pat+=p;
+	cout<<"Enter number of cheese slices"<<endl;
+	cin>>c;
+	*che+=c;
+}
 };
-class station1 : virtual public functions{
+class station1 : public virtual functions{
 	public:
-		int n,bun,pat,che;
+		int n,bun,pat,che,sales1;
+		station1()
+		{
+			bun=2;
+			pat = 1;
+			che = 2;
+		}
 	void st1()
 	{
-		bun,pat,che=2;
+		//bun,pat,che =2;
 		while(1){
 		n=mainmenu();
 			switch(n)
 		{
 			case 1:	
-			rec();
+			rec(&bun,&pat,&che);
 				break;
 			case 2:
-				order(bun,pat,che);
+				sales1=order(&bun,&pat,&che);
 				break;
 			case 3:	
 				dstock(bun,pat,che);
@@ -127,32 +146,25 @@ class station1 : virtual public functions{
       	}
       }
 	}
-	void rec()
-{
-	cout<<"Enter number of buns"<<endl;
-	cin>>bun;
-	bun+=bun;
-	cout<<"Enter number of pattes"<<endl;
-	cin>>pat;
-	pat+=pat;
-	cout<<"Enter number of cheese slices"<<endl;
-	cin>>che;
-	che+=che;
-}
 };
-class station2 : virtual public functions{
-	int n,bun,pat,che;
+class station2 :  public virtual functions{
 	public:
+		int n,bun,pat,che,sales2;
+		station2(){
+			bun=2;
+			pat = 1;
+			che = 2;
+		}
 		void st2()
 	{
 		while(1){
 		n=mainmenu();
 			switch(n)
 		{
-			case 1:	rec();
+			case 1:	rec(&bun,&pat,&che);
 				break;
 			case 2:
-				order(bun,pat,che);
+				sales2=order(&bun,&pat,&che);
 				break;
 			case 3:	
 				dstock(bun,pat,che);
@@ -162,32 +174,25 @@ class station2 : virtual public functions{
       	}
       }
 	}
-			void rec()
-{
-	cout<<"Enter number of buns"<<endl;
-	cin>>bun;
-	bun+=bun;
-	cout<<"Enter number of pattes"<<endl;
-	cin>>pat;
-	pat+=pat;
-	cout<<"Enter number of cheese slices"<<endl;
-	cin>>che;
-	che+=che;
-}
 };
-class station3 : virtual public functions{
+class station3 : public virtual functions{
 	public:
-	int n,bun,pat,che;
+	int n,bun,pat,che,sales3;
+	station3(){
+			bun=2;
+			pat = 1;
+			che = 2;
+		}
 		void st3()
 	{
 		while(1){
 		n=mainmenu();
 			switch(n)
 		{
-			case 1:	rec();
+			case 1:	rec(&bun,&pat,&che);
 				break;
 			case 2:
-				order(bun,pat,che);
+				sales3=order(&bun,&pat,&che);
 				break;
 			case 3:	
 				dstock(bun,pat,che);
@@ -197,33 +202,26 @@ class station3 : virtual public functions{
       	}
       }
 	}
-void rec()
-{
-	cout<<"Enter number of buns"<<endl;
-	cin>>bun;
-	bun+=bun;
-	cout<<"Enter number of pattes"<<endl;
-	cin>>pat;
-	pat+=pat;
-	cout<<"Enter number of cheese slices"<<endl;
-	cin>>che;
-	che+=che;
-}
 };  
 	
-class station4 : virtual public functions{
+class station4 : public virtual functions{
 	public:
-		int n,bun,pat,che;
+		int n,bun,pat,che,sales4;
+		station4(){
+			bun=2;
+			pat = 1;
+			che = 2;
+		}
 		void st4()
 	{
 		while(1){
 		n=mainmenu();
 			switch(n)
 		{
-			case 1:	rec();
+			case 1:	rec(&bun,&pat,&che);
 				break;
 			case 2:
-				order(bun,pat,che);
+				sales4=order(&bun,&pat,&che);
 				break;
 			case 3:	
 				dstock(bun,pat,che);
@@ -233,21 +231,9 @@ class station4 : virtual public functions{
       	}
       }
 	}
-		void rec()
-{
-	cout<<"Enter number of buns"<<endl;
-	cin>>bun;
-	bun+=bun;
-	cout<<"Enter number of pattes"<<endl;
-	cin>>pat;
-	pat+=pat;
-	cout<<"Enter number of cheese slices"<<endl;
-	cin>>che;
-	che+=che;
-}
+
 };
 class head_office: public station1, public station2, public station3, public station4{
-	public:
 	public:
 		int total,n;
 		display()
@@ -293,6 +279,7 @@ class head_office: public station1, public station2, public station3, public sta
 	station2 s2;
 	station3 s3;
 	station4 s4;
+	head_office h;
 	while(1)
 	{
 		cout << "\n \n \n \n \n \n \n \n  ";
@@ -302,6 +289,9 @@ class head_office: public station1, public station2, public station3, public sta
 		cout << endl << endl;
 			switch(st)
 		{
+			case 0:
+				h.display();
+				break;
 			case 1:
 				s1.st1();
 				break;
